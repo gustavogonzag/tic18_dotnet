0ab7e664258435682a30202ac282efe22b16426b
Gustavo Gonzaga <gustavosg91@gmail.com>
2023-11-09 16:46:49 -0300

    configurando ambiente da aula

---
+++ Semana1/PrimeiroPrograma/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region tipo de dados
+
+int tipoInteiro;
+double tipoDouble;
+string tipoString;
+long tipoLong;
+
+tipoInteiro = int.MaxValue;
+
+#endregion
