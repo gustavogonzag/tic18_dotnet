5b0433259b3c488fe3e22003ed20f55214d6627c
Gustavo Gonzaga <gustavosg91@gmail.com>
2023-11-30 16:21:03 -0300

    fix: formatacao

--- AV-T3/Solucao/Program.cs
+++ AV-T3/Solucao/Program.cs
@@ -2,125 +2,193 @@
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
 
-class Pessoa{
-        public string nome{get; protected set;}
-        protected DateTime DataNascimento;
-        public string CPF { get; protected set;}
-        public int idade{get; protected set;}
+class Pessoa
+{
+    public string nome { get; protected set; }
+    protected DateTime DataNascimento;
+    public string CPF { get; protected set; }
+    public int idade { get; protected set; }
 
-        public Pessoa(string _nome, int _idade, DateTime _dataNascimento, string _CPF){
+    public Pessoa(string _nome, int _idade, DateTime _dataNascimento, string _CPF)
+    {
+        this.nome = _nome;
+        this.idade = _idade;
+        this.DataNascimento = _dataNascimento;
+        this.CPF = _CPF;
+    }
+
+    public void imprime()
+    {
+        Console.WriteLine($"{this.nome} - {this.idade}");
+    }
+
+    public void setNome(string _nome)
+    {
+        if (_nome.Length > 0)
+        {
             this.nome = _nome;
-            this.idade = _idade;
-            this.DataNascimento = _dataNascimento;
-            this.CPF = _CPF;
         }
-
-        public void imprime(){
-            Console.WriteLine($"{this.nome} - {this.idade}");
-        }
-
-        public void setNome(string _nome){
-            if(_nome.Length > 0){
-                this.nome = _nome;
-            } else {
-                throw new ArgumentException("Nome inválido");
-            }
-        }
-
-        public void setIdade(int _idade){
-            if(_idade < 0){
-                this.idade = 0;
-                return;
-            } else {
-                throw new ArgumentException("Idade inválida");
-            }
-        }
-
-        public void setDataNascimento(DateTime _dataNascimento){
-            if(_dataNascimento < DateTime.Now){
-                this.DataNascimento = _dataNascimento;
-            } else {
-                throw new ArgumentException("Data inválida");
-            }
-        }
-
-        public void setCPF(string _CPF){
-            if(_CPF.Length == 11 && _CPF.All(c => char.IsDigit(c))){
-                this.CPF = _CPF;
-            } else {
-                throw new ArgumentException("CPF inválido");
-            }
+        else
+        {
+            throw new ArgumentException("Nome inválido");
         }
     }
 
-class Advogado : Pessoa {
+    public void setIdade(int _idade)
+    {
+        if (_idade < 0)
+        {
+            this.idade = 0;
+            return;
+        }
+        else
+        {
+            throw new ArgumentException("Idade inválida");
+        }
+    }
+
+    public void setDataNascimento(DateTime _dataNascimento)
+    {
+        if (_dataNascimento < DateTime.Now)
+        {
+            this.DataNascimento = _dataNascimento;
+        }
+        else
+        {
+            throw new ArgumentException("Data inválida");
+        }
+    }
+
+    protected void setCPF(string _CPF)
+    {
+        if (_CPF.Length == 11 && _CPF.All(c => char.IsDigit(c)))
+        {
+            this.CPF = _CPF;
+        }
+        else
+        {
+            throw new ArgumentException("CPF inválido");
+        }
+    }
+}
+
+class Advogado : Pessoa
+{
     public string CNA;
 
-    public Advogado(string _nome, int _idade, DateTime _dataNascimento, string _CPF, string _CNA) : base(_nome, _idade, _dataNascimento, _CPF){
+    public Advogado(string _nome, int _idade, DateTime _dataNascimento, string _CPF, string _CNA) : base(_nome, _idade, _dataNascimento, _CPF)
+    {
         this.CNA = _CNA;
     }
 
-    public string getCNA(){
+    public string getCNA()
+    {
         return this.CNA;
     }
 
-    protected void setCNA(string _CNA){
-        if(_CNA.Length > 0){
+    protected void setCPF(string _CPF)
+    {
+        if (_CPF.Length == 11 && _CPF.All(c => char.IsDigit(c)))
+        {
+            this.CPF = _CPF;
+        }
+        else
+        {
+            throw new ArgumentException("CPF inválido");
+        }
+    }
+
+    protected void setCNA(string _CNA)
+    {
+        if (_CNA.Length > 0)
+        {
             this.CNA = _CNA;
-        } else {
+        }
+        else
+        {
             throw new ArgumentException("CNA inválido");
         }
     }
 
 }
 
-class Cliente : Pessoa {
-    public string _estadoCivil {get; protected set;}
-    public string _profissao{get; protected set;}
+class Cliente : Pessoa
+{
+    public string _estadoCivil { get; protected set; }
+    public string _profissao { get; protected set; }
 
-    public Cliente(string _nome, int _idade, DateTime _dataNascimento, string _CPF, string _estadoCivil, string _profissao) : base(_nome, _idade, _dataNascimento, _CPF){
+    public Cliente(string _nome, int _idade, DateTime _dataNascimento, string _CPF, string _estadoCivil, string _profissao) : base(_nome, _idade, _dataNascimento, _CPF)
+    {
         this._estadoCivil = _estadoCivil;
         this._profissao = _profissao;
     }
 
-    public string getEstadoCivil(){
+    public string getEstadoCivil()
+    {
         return this._estadoCivil;
     }
 
-    public void setEstadoCivil(string _estadoCivil){
-        if(_estadoCivil.Length > 0){
+    public void setEstadoCivil(string _estadoCivil)
+    {
+        if (_estadoCivil.Length > 0)
+        {
             this._estadoCivil = _estadoCivil;
-        } else {
+        }
+        else
+        {
             throw new ArgumentException("Estado civil inválido");
         }
     }
 
-    public string getProfissao(){
+    public string getProfissao()
+    {
         return this._profissao;
     }
 
-    public void setProfissao(string _profissao){
-        if(_profissao.Length > 0){
+    public void setProfissao(string _profissao)
+    {
+        if (_profissao.Length > 0)
+        {
             this._profissao = _profissao;
-        } else {
+        }
+        else
+        {
             throw new ArgumentException("Profissão inválida");
+        }
+    }
+
+    protected void setCPF(string _CPF)
+    {
+        if (_CPF.Length == 11 && _CPF.All(c => char.IsDigit(c)))
+        {
+            this.CPF = _CPF;
+        }
+        else
+        {
+            throw new ArgumentException("CPF inválido");
         }
     }
 }
 
-class ListaAdvogados{
+class ListaAdvogados
+{
     public List<Advogado> listaAdvogados = new List<Advogado>();
-    public void AdicionarAdvogado(Advogado advogado){
-        if(listaAdvogados.Any((c => c.CPF == advogado.CPF)) || listaAdvogados.Any((c => c.CNA == advogado.CNA))){
-                throw new Exception("Dados de advogado já cadastrados");
-            }
+    public void AdicionarAdvogado(Advogado advogado)
+    {
+        if (listaAdvogados.Any((c => c.CPF == advogado.CPF)) || listaAdvogados.Any((c => c.CNA == advogado.CNA)))
+        {
+            throw new Exception("Dados de advogado já cadastrados");
+        }
         this.listaAdvogados.Add(advogado);
     }
 
-    public List<Advogado> AdvogadosPorIdades(int _idade1, int _idade2){
+    public List<Advogado> AdvogadosPorIdades(int _idade1, int _idade2)
+    {
         List<Advogado> advogadosFilterIdade = new List<Advogado>();
-        foreach(Advogado advogado in this.listaAdvogados){
-            if(advogado.idade >= _idade1 && advogado.idade <= _idade2){
+        foreach (Advogado advogado in this.listaAdvogados)
+        {
+            if (advogado.idade >= _idade1 && advogado.idade <= _idade2)
+            {
                 advogadosFilterIdade.Add(advogado);
             }
         }
@@ -128,48 +196,62 @@ class ListaAdvogados{
     }
 }
 
-class ListaClientes{
+class ListaClientes
+{
     public List<Cliente> listaClientes = new List<Cliente>();
-    public void AdicionarCliente(Cliente cliente){
-        if(listaClientes.Any((c => c.CPF == cliente.CPF))){
-                throw new Exception("Dados de cliente já cadastrados");
-            }
+    public void AdicionarCliente(Cliente cliente)
+    {
+        if (listaClientes.Any((c => c.CPF == cliente.CPF)))
+        {
+            throw new Exception("Dados de cliente já cadastrados");
+        }
         this.listaClientes.Add(cliente);
     }
 
-    public List<Cliente> ClientesPorIdades(int _idade1, int _idade2){
+    public List<Cliente> ClientesPorIdades(int _idade1, int _idade2)
+    {
         List<Cliente> clientesFilterIdade = new List<Cliente>();
-        foreach(Cliente cliente in this.listaClientes){
-            if(cliente.idade >= _idade1 && cliente.idade <= _idade2){
+        foreach (Cliente cliente in this.listaClientes)
+        {
+            if (cliente.idade >= _idade1 && cliente.idade <= _idade2)
+            {
                 clientesFilterIdade.Add(cliente);
             }
         }
         return clientesFilterIdade;
     }
 
-    public List<Cliente> ClientesPorEstadoCivil(string _estadoCivil){
+    public List<Cliente> ClientesPorEstadoCivil(string _estadoCivil)
+    {
         List<Cliente> clientesFilterEstadoCivil = new List<Cliente>();
-        foreach(Cliente cliente in this.listaClientes){
-            if(cliente._estadoCivil == _estadoCivil){
+        foreach (Cliente cliente in this.listaClientes)
+        {
+            if (cliente._estadoCivil == _estadoCivil)
+            {
                 clientesFilterEstadoCivil.Add(cliente);
             }
         }
         return clientesFilterEstadoCivil;
     }
 
-    public List<Cliente> ClientesPorProfissao(string _profissao){
+    public List<Cliente> ClientesPorProfissao(string _profissao)
+    {
         List<Cliente> clientesFilterProfissao = new List<Cliente>();
-        foreach(Cliente cliente in this.listaClientes){
-            if(cliente._profissao == _profissao){
+        foreach (Cliente cliente in this.listaClientes)
+        {
+            if (cliente._profissao == _profissao)
+            {
                 clientesFilterProfissao.Add(cliente);
             }
         }
         return clientesFilterProfissao;
     }
 
-    public List<Cliente> ClienteAlfaOrdenados(){
+    public List<Cliente> ClienteAlfaOrdenados()
+    {
         List<Cliente> clientesAlfa = new List<Cliente>();
-        foreach(Cliente cliente in this.listaClientes){
+        foreach (Cliente cliente in this.listaClientes)
+        {
             clientesAlfa.Add(cliente);
         }
         return clientesAlfa.OrderBy(c => c.nome).ToList();
