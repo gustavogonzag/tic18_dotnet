ae33d6badb919005f50e0dfdb530218b56b937a4
Gustavo Gonzaga <gustavosg91@gmail.com>
2023-12-03 14:31:37 -0300

    Respostas das PIs

---
+++ Semana2/PI-02/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static List<Tarefa> listaTarefas = new List<Tarefa>();
+
+    static void Main()
+    {
+        while (true)
+        {
+            MostrarMenu();
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    CriarTarefa();
+                    break;
+                case "2":
+                    ListarTarefas();
+                    break;
+                case "3":
+                    ListarTarefasConcluidas();
+                    break;
+                case "4":
+                    ListarTarefasPendentes();
+                    break;
+                case "5":
+                    MarcarTarefaConcluida();
+                    break;
+                case "6":
+                    ExcluirTarefa();
+                    break;
+                case "7":
+                    PesquisarTarefa();
+                    break;
+                case "8":
+                    MostrarEstatisticas();
+                    break;
+                case "9":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    static void MostrarMenu()
+    {
+        Console.WriteLine("\n=== Sistema de Gerenciamento de Tarefas ===");
+        Console.WriteLine("1. Criar Tarefa");
+        Console.WriteLine("2. Listar Todas as Tarefas");
+        Console.WriteLine("3. Listar Tarefas Concluídas");
+        Console.WriteLine("4. Listar Tarefas Pendentes");
+        Console.WriteLine("5. Marcar Tarefa como Concluída");
+        Console.WriteLine("6. Excluir Tarefa");
+        Console.WriteLine("7. Pesquisar Tarefa por Palavra-chave");
+        Console.WriteLine("8. Mostrar Estatísticas");
+        Console.WriteLine("9. Sair");
+        Console.Write("Escolha uma opção: ");
+    }
+
+    static void CriarTarefa()
+    {
+        Console.WriteLine("\n=== Criar Tarefa ===");
+        Console.Write("Título: ");
+        string titulo = Console.ReadLine();
+        Console.Write("Descrição: ");
+        string descricao = Console.ReadLine();
+        Console.Write("Data de Vencimento (dd/MM/yyyy): ");
+        if (DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime dataVencimento))
+        {
+            Tarefa novaTarefa = new Tarefa(titulo, descricao, dataVencimento);
+            listaTarefas.Add(novaTarefa);
+            Console.WriteLine("Tarefa criada com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Formato de data inválido. Tarefa não criada.");
+        }
+    }
+
+    static void ListarTarefas()
+    {
+        Console.WriteLine("\n=== Lista de Todas as Tarefas ===");
+        foreach (var tarefa in listaTarefas)
+        {
+            Console.WriteLine(tarefa);
+        }
+    }
+
+    static void ListarTarefasConcluidas()
+    {
+        Console.WriteLine("\n=== Lista de Tarefas Concluídas ===");
+        foreach (var tarefa in listaTarefas.Where(t => t.Concluida))
+        {
+            Console.WriteLine(tarefa);
+        }
+    }
+
+    static void ListarTarefasPendentes()
+    {
+        Console.WriteLine("\n=== Lista de Tarefas Pendentes ===");
+        foreach (var tarefa in listaTarefas.Where(t => !t.Concluida))
+        {
+            Console.WriteLine(tarefa);
+        }
+    }
+
+    static void MarcarTarefaConcluida()
+    {
+        Console.WriteLine("\n=== Marcar Tarefa como Concluída ===");
+        Console.Write("Digite o ID da tarefa: ");
+        if (int.TryParse(Console.ReadLine(), out int id))
+        {
+            Tarefa tarefa = listaTarefas.FirstOrDefault(t => t.Id == id);
+
+            if (tarefa != null)
+            {
+                tarefa.Concluir();
+                Console.WriteLine("Tarefa marcada como concluída!");
+            }
+            else
+            {
+                Console.WriteLine("ID de tarefa não encontrado.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("ID inválido.");
+        }
+    }
+
+    static void ExcluirTarefa()
+    {
+        Console.WriteLine("\n=== Excluir Tarefa ===");
+        Console.Write("Digite o ID da tarefa: ");
+        if (int.TryParse(Console.ReadLine(), out int id))
+        {
+            Tarefa tarefa = listaTarefas.FirstOrDefault(t => t.Id == id);
+
+            if (tarefa != null)
+            {
+                listaTarefas.Remove(tarefa);
+                Console.WriteLine("Tarefa excluída com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("ID de tarefa não encontrado.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("ID inválido.");
+        }
+    }
+
+    static void PesquisarTarefa()
+    {
+        Console.WriteLine("\n=== Pesquisar Tarefa por Palavra-chave ===");
+        Console.Write("Digite a palavra-chave: ");
+        string palavraChave = Console.ReadLine();
+
+        var tarefasEncontradas = listaTarefas.Where(t => t.Titulo.Contains(palavraChave, StringComparison.OrdinalIgnoreCase) || t.Descricao.Contains(palavraChave, StringComparison.OrdinalIgnoreCase));
+
+        if (tarefasEncontradas.Any())
+        {
+            Console.WriteLine("\nTarefas encontradas:");
+            foreach (var tarefa in tarefasEncontradas)
+            {
+                Console.WriteLine(tarefa);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nenhuma tarefa encontrada com a palavra-chave informada.");
+        }
+    }
+
+    static void MostrarEstatisticas()
+    {
+        Console.WriteLine("\n=== Estatísticas ===");
+        Console.WriteLine($"Número total de tarefas: {listaTarefas.Count}");
+        Console.WriteLine($"Número de tarefas concluídas: {listaTarefas.Count(t => t.Concluida)}");
+        Console.WriteLine($"Número de tarefas pendentes: {listaTarefas.Count(t => !t.Concluida)}");
+
+        var tarefaMaisAntiga = listaTarefas.OrderBy(t => t.DataVencimento).FirstOrDefault();
+        var tarefaMaisRecente = listaTarefas.OrderByDescending(t => t.DataVencimento).FirstOrDefault();
+
+        Console.WriteLine($"Tarefa mais antiga: {tarefaMaisAntiga?.DataVencimento.ToString("dd/MM/yyyy") ?? "Nenhuma tarefa"}");
+        Console.WriteLine($"Tarefa mais recente: {tarefaMaisRecente?.DataVencimento.ToString("dd/MM/yyyy") ?? "Nenhuma tarefa"}");
+    }
+}
+
+class Tarefa
+{
+    private static int proximoId = 1;
+
+    public int Id { get; }
+    public string Titulo { get; }
+    public string Descricao { get; }
+    public DateTime DataVencimento { get; }
+    public bool Concluida { get; private set; }
+
+    public Tarefa(string titulo, string descricao, DateTime dataVencimento)
+    {
+        Id = proximoId++;
+        Titulo = titulo;
+        Descricao = descricao;
+        DataVencimento = dataVencimento;
+        Concluida = false;
+    }
+
+    public void Concluir()
+    {
+        Concluida = true;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Título: {Titulo}, Descrição: {Descricao}, Data de Vencimento: {DataVencimento.ToString("dd/MM/yyyy")}, Concluída: {Concluida}";
+    }
+}
---
+++ Semana3/PI-03/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static List<(int Codigo, string Nome, int Quantidade, double Preco)> estoque = new List<(int, string, int, double)>();
+
+    static void Main()
+    {
+        while (true)
+        {
+            MostrarMenu();
+            string opcao = Console.ReadLine();
+
+            try
+            {
+                switch (opcao)
+                {
+                    case "1":
+                        CadastrarProduto();
+                        break;
+                    case "2":
+                        ConsultarProduto();
+                        break;
+                    case "3":
+                        AtualizarEstoque();
+                        break;
+                    case "4":
+                        GerarRelatorios();
+                        break;
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+
+    static void MostrarMenu()
+    {
+        Console.WriteLine("\n=== Sistema de Gerenciamento de Estoque ===");
+        Console.WriteLine("1. Cadastrar Produto");
+        Console.WriteLine("2. Consultar Produto por Código");
+        Console.WriteLine("3. Atualizar Estoque");
+        Console.WriteLine("4. Gerar Relatórios");
+        Console.WriteLine("5. Sair");
+        Console.Write("Escolha uma opção: ");
+    }
+
+    static void CadastrarProduto()
+    {
+        Console.WriteLine("\n=== Cadastrar Produto ===");
+        Console.Write("Código: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        if (estoque.Any(p => p.Codigo == codigo))
+        {
+            throw new InvalidOperationException("Produto com código já cadastrado.");
+        }
+
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Quantidade: ");
+        int quantidade = int.Parse(Console.ReadLine());
+        Console.Write("Preço: ");
+        double preco = double.Parse(Console.ReadLine());
+
+        estoque.Add((codigo, nome, quantidade, preco));
+        Console.WriteLine("Produto cadastrado com sucesso!");
+    }
+
+    static void ConsultarProduto()
+    {
+        Console.WriteLine("\n=== Consultar Produto por Código ===");
+        Console.Write("Digite o código do produto: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        var produto = estoque.FirstOrDefault(p => p.Codigo == codigo);
+
+        if (produto.Equals(default((int, string, int, double))))
+        {
+            throw new ProdutoNaoEncontradoException("Produto não encontrado.");
+        }
+
+        Console.WriteLine($"Código: {produto.Codigo}, Nome: {produto.Nome}, Quantidade: {produto.Quantidade}, Preço: {produto.Preco}");
+    }
+
+    static void AtualizarEstoque()
+    {
+        Console.WriteLine("\n=== Atualizar Estoque ===");
+        Console.Write("Digite o código do produto: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        var produto = estoque.FirstOrDefault(p => p.Codigo == codigo);
+
+        if (produto.Equals(default((int, string, int, double))))
+        {
+            throw new ProdutoNaoEncontradoException("Produto não encontrado.");
+        }
+
+        Console.Write("Digite a quantidade a ser adicionada (negativa para saída): ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        if (produto.Quantidade + quantidade < 0)
+        {
+            throw new InvalidOperationException("Quantidade insuficiente para saída.");
+        }
+
+        produto.Quantidade += quantidade;
+        Console.WriteLine("Estoque atualizado com sucesso!");
+    }
+
+    static void GerarRelatorios()
+    {
+        Console.WriteLine("\n=== Gerar Relatórios ===");
+
+        Console.Write("Informe o limite de quantidade para o relatório 1: ");
+        int limiteQuantidade = int.Parse(Console.ReadLine());
+
+        var relatorio1 = estoque.Where(p => p.Quantidade < limiteQuantidade);
+        Console.WriteLine("\nRelatório 1 - Produtos com quantidade em estoque abaixo do limite:");
+        ImprimirRelatorio(relatorio1);
+
+        Console.Write("\nInforme o valor mínimo para o relatório 2: ");
+        double valorMinimo = double.Parse(Console.ReadLine());
+        Console.Write("Informe o valor máximo para o relatório 2: ");
+        double valorMaximo = double.Parse(Console.ReadLine());
+
+        var relatorio2 = estoque.Where(p => p.Preco >= valorMinimo && p.Preco <= valorMaximo);
+        Console.WriteLine("\nRelatório 2 - Produtos com valor entre o mínimo e o máximo:");
+        ImprimirRelatorio(relatorio2);
+
+        Console.WriteLine("\nRelatório 3 - Valor total do estoque e valor total de cada produto:");
+        var valorTotalEstoque = estoque.Sum(p => p.Quantidade * p.Preco);
+        Console.WriteLine($"Valor total do estoque: {valorTotalEstoque}");
+
+        ImprimirRelatorio(estoque);
+    }
+
+    static void ImprimirRelatorio(IEnumerable<(int Codigo, string Nome, int Quantidade, double Preco)> relatorio)
+    {
+        foreach (var produto in relatorio)
+        {
+            Console.WriteLine($"Código: {produto.Codigo}, Nome: {produto.Nome}, Quantidade: {produto.Quantidade}, Preço: {produto.Preco}");
+        }
+    }
+}
+
+class ProdutoNaoEncontradoException : Exception
+{
+    public ProdutoNaoEncontradoException(string message) : base(message) { }
+}
